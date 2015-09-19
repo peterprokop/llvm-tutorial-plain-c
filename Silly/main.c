5b068e872bbe8f3032eb86a992652d5fc3f477ba
Peter Prokop <prokop.petr@gmail.com>
2015-09-19 17:57:31 +0300

    Initial Commit

---
+++ Silly/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Silly
+//
+//  Created by Peter Prokop on 19/09/15.
+//  Copyright (c) 2015 Peter Prokop. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
